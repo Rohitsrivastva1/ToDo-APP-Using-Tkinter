ff87024e0e2b64246126c3d9832099744981aae1
Rohit-Srivastva <50514493+Rohit-Srivastva@users.noreply.github.com>
2021-12-25 16:45:30 +0530

    commenting

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from tkinter import *
 from typing import Sized
 
 
-gui = Tk()
-gui.geometry("800x900")
-gui.title("ToDo App")
-gui.configure(background='#fef4f4')
+gui = Tk()    #gui initilize
+gui.geometry("800x900")    #used to set window soze
+gui.title("ToDo App")       #used to set Title
+gui.configure(background='#fef4f4')     #used to set background color
 
-Label(gui,text='Sign Up',bg='#fef4f4',   font=("Arial", 25)).grid(row=2, column=15)
+Label(gui,text='Sign Up',bg='#fef4f4',   font=("Arial", 25)).grid(row=2, column=15)     # label used to display text and grid to tell where to display
 
 Label(gui, text="First Name", bg="#fef4f4",width=12).grid(row=3, column=12)
-firstname_field = Entry(gui).grid(row=3, column=15, ipadx="100")
+firstname_field = Entry(gui).grid(row=3, column=15, ipadx="100")              # Entry used to put input box
 Label(gui, text="Last Name ", bg="#fef4f4").grid(row=5, column=12)
 lastname_field = Entry(gui).grid(row=5, column=15, ipadx="100")
 Label(gui, text="Age", bg="#fef4f4").grid(row=6, column=12)

c18e7b219561f0918a7b4219252de1b4b84a997a
Rohit Srivastava <70742576+Rohitsrivastva1@users.noreply.github.com>
2021-12-30 13:43:17 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,23 +1,35 @@
 from tkinter import *
-from tkinter import font
-from typing import Sized
 
-gui = Tk()   
-gui.geometry("800x900")    
-gui.title("login page")  
-gui.configure(background='#fef4f4') 
 
-def getvals():
+
+def startLogin(gui1): 
+    gui1.geometry("800x900")    
+    gui1.title("login page")  
+    gui1.configure(background='#fef4f4') 
+    
+    Label(gui1, text="login page", font=("Arial", 25),bg="#fef4f4").grid(row=0, column=15)
+
+    Username = Label(gui1, text="Name", font=("Bold", 15),bg="#fef4f4").grid(row=1, column=2)
+
+    password = Label(gui1, text="Password", font=("Bold", 15),bg="#fef4f4").grid(row=2, column=2)
+
+    username_field = Entry(gui1).grid(row=1, column=12, ipadx="50")  
+    password_field = Entry(gui1,show="*").grid(row=2, column=12, ipadx="50")  
+
+    Button(text="log in", command=lambda:getvals(gui1)).grid(row=5, column=12)
+
+
+def getvals(gui1):
+    gui1.destroy()
     print("Accepted")
 
-Label(gui, text="login page", font=("Arial", 25)).grid(row=0, column=15)
 
-Username = Label(gui, text="Name", font=("Bold", 15)).grid(row=1, column=2)
 
-password = Label(gui, text="Password", font=("Bold", 15)).grid(row=2, column=2)
 
-username_field = Entry(gui).grid(row=1, column=12, ipadx="50")  
-password_field = Entry(gui,show="*").grid(row=2, column=12, ipadx="50")  
 
-Button(text="log in", command=getvals).grid(row=5, column=12)
-gui.mainloop()
+
+def calllogin():
+    print("working")
+    gui1 = Tk()
+    startLogin(gui1)
+    gui1.mainloop
